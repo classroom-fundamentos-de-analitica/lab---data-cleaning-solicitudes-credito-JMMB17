1d80ae99ae0f73b8c0942108b1432e21c60e8fcc
Jan Michael Muñoz Botero <112509633+JMMB17@users.noreply.github.com>
2023-04-10 21:13:21 -0500

    prueba1

--- pregunta.py
+++ pregunta.py
@@ -1,10 +1,8 @@
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
-
 Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizada 
 correctamente. Tenga en cuenta datos faltantes y duplicados.
-
 """
 import pandas as pd
 
@@ -13,8 +11,21 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df = df.drop('Unnamed: 0', axis = 1)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+
+    df.monto_del_credito = df.monto_del_credito.apply(lambda x: str(x).strip('$').replace(',',''))
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+
+    for columna in ['sexo','tipo_de_emprendimiento',
+                    'idea_negocio','barrio','línea_credito']:
+        df[columna] = df[columna].apply(
+                                        lambda s: s.lower().replace('-',' ').replace('_',' ') 
+                                        if type(s) == str else s)
+    df.dropna(axis=0, inplace = True)
+    df.drop_duplicates(inplace=True)
 
     return df
+
+print(clean_data().sexo.value_counts().to_list())
